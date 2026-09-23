122ee16196516bed3de47211e62cc81546564581
dama-nation <adamachide@gmail.com>
2026-09-23 21:56:53 +0100

    Necessary fixes

--- backend/main.js
+++ backend/main.js
@@ -28,14 +28,12 @@ app.use('/api/rating', protectRoute, ratingRoutes)
 app.use('/api/watch-history', protectRoute, watchHistoryRoutes)
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, '/frontend/dist')))
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
     app.get(/(.*)/, (req, res) => {
         res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
     })
 }
-
-
 
 app.listen(PORT, () => {
     console.log(`Server is up and running on http://localhost:${PORT}`)
